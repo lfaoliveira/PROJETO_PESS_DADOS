2b8a9f6e8c9fef02e12fe477896e7f54f1bcb418
LUIS FELIPE DESKTOP <luisfelipearaujo503@gmail.com>
2026-02-09 15:27:09 -0300

    chore

--- src/Models/utils.py
+++ src/Models/utils.py
@@ -14,9 +14,9 @@ def calc_metrics(labels: torch.Tensor, logits: torch.Tensor):
     prec = np.mean(prec) if isinstance(prec, np.ndarray) else float(prec)
     rec = np.mean(rec) if isinstance(rec, np.ndarray) else float(rec)
     f_beta = np.mean(f_beta) if isinstance(f_beta, np.ndarray) else float(f_beta)
-    # Calculate ROC-AUC for binary classification
-    probabilities = torch.softmax(logits, dim=1).numpy(force=True)
+
     # NOTE: disabling ROC for now (need to adapt to binary classes)
+    # probabilities = torch.softmax(logits, dim=1).numpy(force=True)
     # roc_auc = roc_auc_score(labels.numpy(force=True), probabilities[:, 1])
     roc_auc = 0
 
