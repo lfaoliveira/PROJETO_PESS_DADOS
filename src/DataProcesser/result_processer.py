d6373290921a5aad873f555b189a0f7971a65d90
LUIS FELIPE DESKTOP <luisfelipearaujo503@gmail.com>
2026-02-11 16:38:40 -0300

    feat: result processer fits and predicts

--- src/DataProcesser/result_processer.py
+++ src/DataProcesser/result_processer.py
@@ -4,15 +4,20 @@ from Models.error_model import ErrorModel
 
 
 class ResultsProcesser:
+    """ Class that stores Error Model results and predictions dfs"""
     analysis_models: dict[str, ErrorModel]
-    results_dict: dict[str, pd.DataFrame]
+    classifier_pred_df: dict[str, pd.DataFrame] 
 
     def __init__(
         self,
     ) -> None:
         self.analysis_models = {}
-        self.results_dict = {}
+        self.classifier_pred_df = {}
 
     def update(self, name: str, model: ErrorModel, df: pd.DataFrame):
         self.analysis_models[name] = model
-        self.results_dict[name] = df
+        self.classifier_pred_df[name] = df
+
+    def fit_predict(self, name: str, log_predict=True):
+        self.analysis_models[name].fit()
+        self.analysis_models[name].predict(log_predict)
