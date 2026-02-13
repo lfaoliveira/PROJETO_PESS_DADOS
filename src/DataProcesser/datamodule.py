3c6e8fdd537feba7e28f5610d64e6d25e354a8ac
PC_SUAS <luisfelipearaujo503@gmail.com>
2026-02-13 09:41:28 -0300

    refactor: undoing etl folder for DataProcesser folder (makes more sense)

--- src/DataProcesser/datamodule.py
+++ src/DataProcesser/datamodule.py
@@ -1,5 +1,5 @@
 from lightning.pytorch.core.datamodule import LightningDataModule
-from etl.dataset import StrokeDataset
+from DataProcesser.dataset import StrokeDataset
 from torch.utils.data import DataLoader, random_split
 
 
